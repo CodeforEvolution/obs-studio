893afddbe42af6ffdbde80210f41c5cda0593864
CodeforEvolution <secundaja@gmail.com>
2024-06-07 23:53:33 -0500

    libobs-opengl: Finalize Haiku Support Using OSMesa

--- libobs-opengl/gl-haiku.cpp
+++ libobs-opengl/gl-haiku.cpp
@@ -15,153 +15,72 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 
-#include "gl-haiku.h"
 #include "gl-subsystem.h"
+#define GLAPIENTRY APIENTRY
+#include <GL/osmesa.h>
 
 #include <Bitmap.h>
+#include <Screen.h>
+#include <View.h>
 #include <Window.h>
 
-static const int ctx_attribs[] = {
-	EGL_CONTEXT_OPENGL_PROFILE_MASK, EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT,
-	EGL_CONTEXT_MAJOR_VERSION, 3,
-	EGL_CONTEXT_MINOR_VERSION, 3,
-	EGL_NONE,
-};
 
-static int ctx_pbuffer_attribs[] = {
-		EGL_WIDTH, 2,
-		EGL_HEIGHT, 2,
-		EGL_NONE
+struct gl_windowinfo {
+	BView *view;
+	BBitmap *bitmap;
+	OSMesaContext context;
+	gs_texture_t *texture;
+    GLuint fbo;
 };
-
-static const EGLint ctx_config_attribs[] = {
-		EGL_STENCIL_SIZE, 0,
-		EGL_DEPTH_SIZE, 0,
-		EGL_BUFFER_SIZE, 24,
-		EGL_ALPHA_SIZE, 0,
-		EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
-		EGL_SURFACE_TYPE, EGL_WINDOW_BIT | EGL_PBUFFER_BIT,
-		EGL_NONE
-};
-
-//static const EGLint ctx_config_attribs[] = {
-//		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-////		EGL_BLUE_SIZE, 8,
-////		EGL_GREEN_SIZE, 8,
-////		EGL_RED_SIZE, 8,
-////		EGL_DEPTH_SIZE, 8,
-//		EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
-//		EGL_NONE
-//};
 
 struct gl_platform {
-	EGLDisplay edisplay;
-	EGLConfig config;
-	EGLContext context;
-	EGLSurface pbuffer;
+	OSMesaContext context;
+	BBitmap *bitmap;
 };
 
-static const char *
-get_egl_error_string(const EGLint error)
+static OSMesaContext
+gl_context_create(OSMesaContext shareContext)
 {
-	switch (error) {
-#define OBS_EGL_CASE_ERROR(e) \
-	case e:               \
-		return #e;
-		OBS_EGL_CASE_ERROR(EGL_SUCCESS)
-		OBS_EGL_CASE_ERROR(EGL_NOT_INITIALIZED)
-		OBS_EGL_CASE_ERROR(EGL_BAD_ACCESS)
-		OBS_EGL_CASE_ERROR(EGL_BAD_ALLOC)
-		OBS_EGL_CASE_ERROR(EGL_BAD_ATTRIBUTE)
-		OBS_EGL_CASE_ERROR(EGL_BAD_CONTEXT)
-		OBS_EGL_CASE_ERROR(EGL_BAD_CONFIG)
-		OBS_EGL_CASE_ERROR(EGL_BAD_CURRENT_SURFACE)
-		OBS_EGL_CASE_ERROR(EGL_BAD_DISPLAY)
-		OBS_EGL_CASE_ERROR(EGL_BAD_SURFACE)
-		OBS_EGL_CASE_ERROR(EGL_BAD_MATCH)
-		OBS_EGL_CASE_ERROR(EGL_BAD_PARAMETER)
-		OBS_EGL_CASE_ERROR(EGL_BAD_NATIVE_PIXMAP)
-		OBS_EGL_CASE_ERROR(EGL_BAD_NATIVE_WINDOW)
-		OBS_EGL_CASE_ERROR(EGL_CONTEXT_LOST)
-#undef OBS_EGL_CASE_ERROR
-	default:
-		return "Unknown";
-	}
-}
+	int attributes[] = {
+		OSMESA_FORMAT, OSMESA_BGRA,
+		OSMESA_DEPTH_BITS, 0,
+		OSMESA_STENCIL_BITS, 0,
+		OSMESA_ACCUM_BITS, 0,
+		OSMESA_PROFILE, OSMESA_CORE_PROFILE,
+		OSMESA_CONTEXT_MAJOR_VERSION, 3,
+		OSMESA_CONTEXT_MINOR_VERSION, 3,
+		0, 0
+	};
 
-bool
-gl_context_create(struct gl_platform *plat)
-{
-	int frame_buf_config_count = 0;
-	EGLDisplay edisplay = EGL_NO_DISPLAY;
-	EGLConfig config = NULL;
-	EGLContext context = EGL_NO_CONTEXT;
-	int egl_min = 0, egl_maj = 0;
-	bool success = false;
-
-	eglBindAPI(EGL_OPENGL_API);
-
-	// Miraculously, this will get Haiku's EGL platform
-	edisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-
-	if (edisplay == EGL_NO_DISPLAY) {
-		blog(LOG_ERROR, "Failed to get EGL display using eglGetDisplay");
-		return false;
+	OSMesaContext context = OSMesaCreateContextAttribs(attributes, shareContext);
+	if (context == nullptr) {
+		blog(LOG_ERROR, "Failed to create OSMesaContext");
+		return nullptr;
 	}
 
-	if (!eglInitialize(edisplay, &egl_maj, &egl_min)) {
-		blog(LOG_ERROR, "Failed to initialize EGL: %s", get_egl_error_string(eglGetError()));
-		return false;
-	}
-
-	if (!eglChooseConfig(edisplay, ctx_config_attribs, &config, 1, &frame_buf_config_count)) {
-		blog(LOG_ERROR, "Unable to find suitable EGL config: %s", get_egl_error_string(eglGetError()));
-		goto error;
-	}
-
-	context = eglCreateContext(edisplay, config, EGL_NO_CONTEXT, ctx_attribs);
-	if (context == EGL_NO_CONTEXT) {
-		blog(LOG_ERROR, "Unable to create EGL context: %s", get_egl_error_string(eglGetError()));
-		goto error;
-	}
-
-	plat->pbuffer = eglCreatePbufferSurface(edisplay, config, ctx_pbuffer_attribs);
-	if (EGL_NO_SURFACE == plat->pbuffer) {
-		blog(LOG_ERROR, "Failed to create OpenGL pbuffer: %s", get_egl_error_string(eglGetError()));
-		goto error;
-	}
-
-	plat->edisplay = edisplay;
-	plat->config = config;
-	plat->context = context;
-
-	success = true;
-	blog(LOG_DEBUG, "Created EGLDisplay %p", plat->edisplay);
-
-error:
-	if (!success) {
-		if (EGL_NO_CONTEXT != context)
-			eglDestroyContext(edisplay, context);
-		eglTerminate(edisplay);
-	}
-
-	return success;
+	return context;
 }
 
 void
 gl_context_destroy(struct gl_platform *plat)
 {
-	eglMakeCurrent(plat->edisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-	eglDestroyContext(plat->edisplay, plat->context);
+	OSMesaMakeCurrent(nullptr, nullptr, GL_UNSIGNED_BYTE, 0, 0);
+	OSMesaDestroyContext(plat->context);
+	plat->context = nullptr;
 }
 
 struct gl_windowinfo *
 gl_windowinfo_create(const struct gs_init_data *info)
 {
+	if (!info || !info->window.window)
+		return nullptr;
+
 	auto windowInfo = static_cast<gl_windowinfo *>(bzalloc(sizeof(struct gl_windowinfo)));
+	if (windowInfo == nullptr)
+		return nullptr;
 
 	auto parentWindow = static_cast<BWindow *>(info->window.window);
-	windowInfo->surfaceView = parentWindow->ChildAt(0);
+	windowInfo->view = parentWindow->ChildAt(0);
 
 	return windowInfo;
 }
@@ -177,52 +96,62 @@ gl_platform_create(gs_device_t *device, uint32_t adapter)
 {
 	UNUSED_PARAMETER(adapter);
 
-	if (!gladLoadEGL()) {
-		blog(LOG_ERROR, "Unable to load EGL entry functions.");
-		return NULL;
-	}
-
 	auto plat = static_cast<gl_platform *>(bmalloc(sizeof(struct gl_platform)));
+	if (plat == nullptr)
+		return nullptr;
 
-	/* We assume later that cur_swap is already set. */
-	device->plat = plat;
-
-	if (!gl_context_create(plat)) {
-		blog(LOG_ERROR, "Failed to create context!");
-		goto fail_context_create;
+	OSMesaContext context = gl_context_create(nullptr);
+	if (context == nullptr) {
+		blog(LOG_ERROR, "gl_context_create failed");
+		bfree(plat);
+		return nullptr;
 	}
 
-	if (!eglMakeCurrent(plat->edisplay, plat->pbuffer, plat->pbuffer, plat->context)) {
-		blog(LOG_ERROR, "Failed to make context current: %s", get_egl_error_string(eglGetError()));
-		goto fail_make_current;
+	BScreen screen;
+	BBitmap *renderBitmap = new(std::nothrow) BBitmap(BRect(0, 0, screen.Frame().IntegerWidth(), screen.Frame().IntegerHeight()), B_RGBA32);
+	if (renderBitmap == nullptr || renderBitmap->InitCheck() != B_OK) {
+		blog(LOG_ERROR, "Failed to allocate the renderBitmap");
+		OSMesaDestroyContext(context);
+		bfree(plat);
+		return nullptr;
+	}
+
+	if (!OSMesaMakeCurrent(context, renderBitmap->Bits(), GL_UNSIGNED_BYTE, renderBitmap->Bounds().IntegerWidth() + 1, renderBitmap->Bounds().IntegerHeight() + 1)) {
+		delete renderBitmap;
+		OSMesaDestroyContext(context);
+		bfree(plat);
+		return nullptr;
 	}
 
 	if (!gladLoadGL()) {
 		blog(LOG_ERROR, "Failed to load OpenGL entry functions.");
-		goto fail_load_gl;
+		delete renderBitmap;
+		OSMesaDestroyContext(context);
+		bfree(plat);
+		return nullptr;
 	}
 
-	goto success;
+	device->plat = plat;
+	plat->context = context;
+	plat->bitmap = renderBitmap;
 
-fail_make_current:
-	gl_context_destroy(plat);
-fail_context_create:
-fail_load_gl:
-	bfree(plat);
-	plat = NULL;
-success:
 	return plat;
 }
 
 void
-gl_platform_destroy(struct gl_platform *plat)
+gl_platform_destroy(struct gl_platform *platform)
 {
-	if (!plat)
+	if (platform == nullptr)
 		return;
 
-	gl_context_destroy(plat);
-	eglTerminate(plat->edisplay);
-	bfree(plat);
+	OSMesaMakeCurrent(nullptr, nullptr, GL_UNSIGNED_BYTE, 0, 0);
+	OSMesaDestroyContext(platform->context);
+	platform->context = nullptr;
+
+	delete platform->bitmap;
+	platform->bitmap = nullptr;
+
+	bfree(platform);
 }
 
 bool
@@ -230,17 +159,41 @@ gl_platform_init_swapchain(struct gs_swap_chain *swap)
 {
 	const struct gl_platform *plat = swap->device->plat;
 
-	swap->wi->bitmapHook = new(std::nothrow) SwapChainBitmapHook(swap->wi);
-	if (swap->wi->bitmapHook == NULL)
+	OSMesaContext parentContext = plat->context;
+	BBitmap *renderBitmap = plat->bitmap;
+
+	OSMesaContext swapContext = gl_context_create(parentContext);
+	if (swapContext == nullptr)
 		return false;
 
-	const EGLSurface surface = eglCreateWindowSurface(plat->edisplay, plat->config, reinterpret_cast<EGLNativeWindowType>(swap->wi->bitmapHook), 0);
-	if (surface == EGL_NO_SURFACE) {
-		blog(LOG_ERROR, "Cannot get window EGL surface: %s", get_egl_error_string(eglGetError()));
-		delete swap->wi->bitmapHook;
+    struct gs_init_data *init_data = &swap->info;
+
+	BBitmap *swapBitmap = new(std::nothrow) BBitmap(BRect(B_ORIGIN, BSize(init_data->cx, init_data->cy)), B_RGBA32);
+	if (swapBitmap == nullptr) {
+		OSMesaDestroyContext(swapContext);
 		return false;
 	}
-	swap->wi->surface = surface;
+
+	OSMesaMakeCurrent(parentContext, renderBitmap->Bits(), GL_UNSIGNED_BYTE, renderBitmap->Bounds().IntegerWidth() + 1, renderBitmap->Bounds().IntegerHeight() + 1);
+
+	swap->wi->texture = device_texture_create(swap->device, init_data->cx, init_data->cy, init_data->format, 1,
+                                                  nullptr, GS_RENDER_TARGET);
+	glFlush();
+
+	OSMesaMakeCurrent(nullptr, nullptr, GL_UNSIGNED_BYTE, 0, 0);
+
+	OSMesaMakeCurrent(swapContext, swapBitmap->Bits(), GL_UNSIGNED_BYTE, swapBitmap->Bounds().IntegerWidth() + 1, swapBitmap->Bounds().IntegerHeight() + 1);
+
+	gl_gen_framebuffers(1, &swap->wi->fbo);
+	gl_bind_framebuffer(GL_FRAMEBUFFER, swap->wi->fbo);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, swap->wi->texture->texture, 0);
+	gl_success("glFrameBufferTexture2D");
+	glFlush();
+
+	OSMesaMakeCurrent(nullptr, nullptr, GL_UNSIGNED_BYTE, 0, 0);
+
+	swap->wi->context = swapContext;
+	swap->wi->bitmap = swapBitmap;
 
 	return true;
 }
@@ -248,40 +201,54 @@ gl_platform_init_swapchain(struct gs_swap_chain *swap)
 void
 gl_platform_cleanup_swapchain(struct gs_swap_chain *swap)
 {
-	eglDestroySurface(swap->device->plat->edisplay, swap->wi->surface);
-	swap->wi->surface = EGL_NO_SURFACE;
+    OSMesaContext parentContext = swap->device->plat->context;
+	BBitmap *parentBitmap = swap->device->plat->bitmap;
 
-	delete swap->wi->bitmapHook;
-	swap->wi->bitmapHook = NULL;
+    OSMesaContext swapContext = swap->wi->context;
+	BBitmap* swapBitmap = swap->wi->bitmap;
+
+	OSMesaMakeCurrent(swapContext, swapBitmap->Bits(), GL_UNSIGNED_BYTE, swapBitmap->Bounds().IntegerWidth() + 1, swapBitmap->Bounds().IntegerHeight() + 1);
+
+    gl_delete_framebuffers(1, &swap->wi->fbo);
+    glFlush();
+
+    OSMesaMakeCurrent(nullptr, nullptr, GL_UNSIGNED_BYTE, 0, 0);
+
+	OSMesaMakeCurrent(parentContext, parentBitmap->Bits(), GL_UNSIGNED_BYTE, parentBitmap->Bounds().IntegerWidth() + 1, parentBitmap->Bounds().IntegerHeight() + 1);
+
+    gs_texture_destroy(swap->wi->texture);
+    glFlush();
+
+    OSMesaMakeCurrent(nullptr, nullptr, GL_UNSIGNED_BYTE, 0, 0);
+
+	OSMesaDestroyContext(swapContext);
+	swap->wi->context = nullptr;
+
+	delete swap->wi->bitmap;
+	swap->wi->bitmap = nullptr;
 }
 
 void device_enter_context(gs_device_t *device)
 {
-	const EGLContext context = device->plat->context;
-	const EGLDisplay display = device->plat->edisplay;
-	const EGLSurface surface = (device->cur_swap)
-					   ? device->cur_swap->wi->surface
-					   : device->plat->pbuffer;
+	struct gl_platform *plat = device->plat;
+	OSMesaContext context = plat->context;
+	BBitmap *bitmap = plat->bitmap;
 
-	if (!eglMakeCurrent(display, surface, surface, context))
-		blog(LOG_ERROR, "Failed to make context current: %s", get_egl_error_string(eglGetError()));
+	OSMesaMakeCurrent(context, bitmap->Bits(), GL_UNSIGNED_BYTE, bitmap->Bounds().IntegerWidth() + 1, bitmap->Bounds().IntegerHeight() + 1);
 }
 
 void
 device_leave_context(gs_device_t *device)
 {
-	device->cur_render_target = NULL;
-	device->cur_zstencil_buffer = NULL;
-	device->cur_vertex_buffer = NULL;
-	device->cur_index_buffer = NULL;
-	device->cur_swap = NULL;
-	device->cur_fbo = NULL;
+    glFlush();
+	OSMesaMakeCurrent(nullptr, nullptr, GL_UNSIGNED_BYTE, 0, 0);
 
-	const EGLDisplay display = device->plat->edisplay;
-
-	if (!eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE,  EGL_NO_CONTEXT)) {
-		blog(LOG_ERROR, "Failed to reset current context: %s", get_egl_error_string(eglGetError()));
-	}
+    device->cur_vertex_buffer = nullptr;
+    device->cur_index_buffer = nullptr;
+    device->cur_render_target = nullptr;
+    device->cur_zstencil_buffer = nullptr;
+    device->cur_swap = nullptr;
+    device->cur_fbo = nullptr;
 }
 
 void *
@@ -293,26 +260,21 @@ device_get_device_obj(gs_device_t *device)
 void
 gl_getclientsize(const struct gs_swap_chain *swap, uint32_t *width, uint32_t *height)
 {
-	BRect frame = swap->wi->surfaceView->Frame();
-
-	if (frame.IsValid()) {
-		*width = frame.IntegerWidth() + 1;
-		*height = frame.IntegerHeight() + 1;
-	}
+//	BRect frame = swap->wi->view->Frame();
+//
+//	if (frame.IsValid()) {
+//		*width = frame.IntegerWidth() + 1;
+//		*height = frame.IntegerHeight() + 1;
+//	}
+    if (width)
+        *width = swap->info.cx;
+    if (height)
+        *height = swap->info.cy;
 }
 
 void
 gl_update(gs_device_t *device)
 {
-//	Display *display = device->plat->xdisplay;
-//	xcb_window_t window = device->cur_swap->wi->window;
-//
-//	uint32_t values[] = {device->cur_swap->info.cx,
-//			     device->cur_swap->info.cy};
-//
-//	xcb_configure_window(XGetXCBConnection(display), window,
-//			     XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
-//			     values);
 
 	UNUSED_PARAMETER(device);
 	// Nothing to do?
@@ -321,11 +283,8 @@ gl_update(gs_device_t *device)
 void
 gl_clear_context(gs_device_t *device)
 {
-	EGLDisplay display = device->plat->edisplay;
-
-	if (!eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE,  EGL_NO_CONTEXT)) {
-		blog(LOG_ERROR, "Failed to reset current context.");
-	}
+	UNUSED_PARAMETER(device);
+	OSMesaMakeCurrent(nullptr, nullptr, GL_UNSIGNED_BYTE, 0, 0);
 }
 
 void
@@ -349,18 +308,12 @@ device_is_present_ready(gs_device_t *device)
 void
 device_present(gs_device_t *device)
 {
-	BView *surfaceView = device->cur_swap->wi->surfaceView;
-	BBitmap *surfaceBitmap = device->cur_swap->wi->surfaceBitmap;
-	if (surfaceView != NULL && surfaceBitmap != NULL && surfaceView->LockLooperWithTimeout(10)) {
-		surfaceView->DrawBitmap(surfaceBitmap, B_ORIGIN);
-		surfaceView->UnlockLooper();
+	BView *view = device->cur_swap->wi->view;
+	BBitmap *bitmap = device->cur_swap->wi->bitmap;
+	if (view != nullptr && bitmap != nullptr && view->LockLooperWithTimeout(100) == B_OK) {
+		view->DrawBitmap(bitmap, B_ORIGIN);
+		view->UnlockLooper();
 	}
-
-	if (eglSwapInterval(device->plat->edisplay, 0) == EGL_FALSE)
-		blog(LOG_ERROR, "eglSwapInterval failed");
-
-	if (!eglSwapBuffers(device->plat->edisplay, device->cur_swap->wi->surface))
-		blog(LOG_ERROR, "Cannot swap EGL buffers: %s", get_egl_error_string(eglGetError()));
 }
 
 bool
